00260d05b5e80ad47ff17e4450f6f18ad9e273bc
LuMarschhausen <lmarschh@cos.ufrj.br>
2024-04-23 12:53:09 -0300

    aaaaa

--- src/indexador.py
+++ src/indexador.py
@@ -1,87 +1,112 @@
-import os
-import csv
-import xml.etree.ElementTree as ET
-import logging
+import pandas as pd
+import numpy as np
 import time
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
+import json
 from collections import defaultdict
 
-# Configuração do logger
-logging.basicConfig(filename='src/logs/indexador.log', level=logging.INFO,
+import logging
+logging.basicConfig(filename='src/logs/indexador.log', level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Pré-processamento texto do documento
-def preprocess_text(text):
-    # Tokenização
-    tokens = word_tokenize(text)
+import xmltodict
+from lxml import etree
 
-    # Remoção de stopwords e tokens com menos de 2 caracteres
-    stop_words = set(stopwords.words('english'))
-    tokens_sem_stopwords = [token for token in tokens if token.lower() not in stop_words and len(token) > 1]
+import json
 
-    return tokens_sem_stopwords
+# Ler um arquivo de configuração
+def read_config(config_file):
+    logging.debug(f'Lendo arquivo de configuracao ({config_file})')
+    with open(config_file, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line.startswith('LEIA'):
+                file_path = line.split('=')[1].strip()
+                df = read_csv(file_path)
 
-# Indexar documentos
-def indexar_documentos(documentos, arquivo_saida):
-    try:
-        # Logging: Início da indexação dos documentos
-        logging.info("Iniciando indexação dos documentos.")
-        start_time = time.time()
+            elif line.startswith('ESCREVA'):
+                file_path = line.split('=')[1].strip()
+                index(df, file_path)
 
-        # Dicionário para armazenar o índice
-        indice = defaultdict(list)
+# Ler um arquivo csv
+def read_csv(csv_file):
+    logging.debug(f'Lendo arquivo CSV ({csv_file})')
+    df = pd.read_csv(csv_file, sep=';', header=None)
+    df.columns = ['TOKEN', 'LIST_DOCUMENTS']
+    
+    # Converter as strings em listas de documentos usando json.loads()
+    df['LIST_DOCUMENTS'] = df['LIST_DOCUMENTS'].apply(json.loads)
+    
+    len_df = len(df)
+    logging.debug(f'Total de tokens na lista invertida: {len_df}')
+    
+    return df
 
-        # Iterar sobre os documentos e indexá-los
-        for i, documento in enumerate(documentos):
-            documento_preprocessado = preprocess_text(documento)
-            for termo in documento_preprocessado:
-                indice[termo].append(i + 1)  # Adiciona o número do documento ao índice
+# Indexador
+def index(df,file):
+    logging.debug(f'Gerando indexador segundo modelo vetorial')
+    dtm = document_term_matrix(df)
+    tf = term_frequency(dtm)
+    idf = inverse_document_frequency(dtm)
+    tfidf = tf_idf(tf,idf)
+    norm = euclidean_norm(tfidf)
+    
+    model = {
+        'idf': idf.to_dict(),
+        'tf_idf' : tfidf.to_dict(),
+        'norm': norm.to_dict()
+    }
 
-        # Escrever o índice no arquivo CSV
-        with open(arquivo_saida, 'w', newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(['Termo', 'Documentos'])
-            for termo, documentos in sorted(indice.items()):
-                writer.writerow([termo, documentos])
+    logging.debug(f'Salvando modelo vetorial em {file}.json')
+    with open(f'RESULT/{file}.json', 'w') as f:
+        json.dump(model, f)
 
-        # Logging: Fim da indexação dos documentos
-        end_time = time.time()
-        elapsed_time = end_time - start_time
-        logging.info(f"Indexação concluída com sucesso. Tempo total: {elapsed_time:.2f} segundos.")
+# Gerar matriz termo documento
+def document_term_matrix(df):
+    logging.debug('Gerando a matriz termo documento')
+    frequency = defaultdict(lambda: defaultdict(int))
 
-    except Exception as e:
-        # Logging: Erro durante a indexação dos documentos
-        logging.error(f"Ocorreu um erro durante a indexação dos documentos: {str(e)}")
+    for token, list_documents in zip(df['TOKEN'], df['LIST_DOCUMENTS']):
+        for doc in list_documents:
+            frequency[doc][token] += 1
 
-# Teste do módulo Indexador
+    df_frequency = pd.DataFrame(frequency).fillna(0)
+    df_frequency = df_frequency.astype(int)
+
+    df_frequency = df_frequency.sort_index()
+    df_frequency = df_frequency.sort_index(axis=1)
+
+    return df_frequency
+
+# Calcular TF (Term Frequency)
+def term_frequency(df):
+    logging.debug('Calculando TF (term frequency)')
+    tf = df.apply(lambda x: x / x.max())
+    return tf
+
+# Calcular IDF (Inverse Document Frequency)
+def inverse_document_frequency(df):
+    logging.debug('Calculando IDF (inverse document frequency)')
+    N = len(df.columns)
+    idf = np.log(N/df.astype(bool).sum(axis=1))
+    return idf
+
+# TF-IDF (Term Frequency - Inverse Document Frequency)
+def tf_idf(tf,idf):
+    logging.debug('Calculando TF-IDF (term frequency - inverse document frequency)')
+    return tf.mul(idf, axis=0)
+
+# Calcular a norma euclidiana
+def euclidean_norm(df):
+    logging.debug(f'Calculando norma euclidiana dos documentos')
+    return np.sqrt((df**2).sum())
+
+def INDEX():
+    logging.debug('Iniciando INDEXADOR')
+    ini = time.time()
+    read_config('config/index.cfg')
+    fim = time.time()
+    logging.debug('Fim do INDEXADOR')
+    logging.debug(f'Tempo de execução do INDEXADOR: {fim-ini:.2f}s')
+
 if __name__ == "__main__":
-    # Arquivo de configuração
-    arquivo_configuracao = "config/index.cfg"
-
-    # Ler as configurações do arquivo de configuração
-    configuracoes = {}
-    with open(arquivo_configuracao, 'r') as f:
-        for linha in f:
-            chave, valor = linha.strip().split('=')
-            configuracoes[chave.strip()] = valor.strip()
-
-    # Obter os caminhos dos arquivos XML a serem processados
-    arquivos_xml = [configuracoes[chave] for chave in configuracoes if chave.startswith("LEIA")]
-
-    # Ler os documentos dos arquivos XML
-    documentos = []
-    for arquivo_xml in arquivos_xml:
-        tree = ET.parse(arquivo_xml)
-        root = tree.getroot()
-        for registro_xml in root.findall('RECORD'):
-            abstract_element = registro_xml.find('ABSTRACT')
-            if abstract_element is not None:
-                texto_abstract = abstract_element.text.strip()
-                documentos.append(texto_abstract)
-
-    # Arquivo de saída
-    arquivo_saida = configuracoes.get("ESCREVA")
-
-    # Indexar os documentos
-    indexar_documentos(documentos, arquivo_saida)
+    INDEX()
